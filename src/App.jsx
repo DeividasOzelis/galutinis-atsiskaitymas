1cbad0a76724aa29f3ae23a9e381eccd9075b8c5
DeividasOzelis <designe88@gmail.com>
2024-03-25 15:32:29 +0200

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Header />
       <main>
-
+        <Home />
       </main>
       <Footer />
     </>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,119 @@
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    padding-block: 50px;
+    > .container{
+        width: 80%;
+        height: 500px;
+        display: flex;
+        > .left{
+            width: 60%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            > p{
+                width: 70ch;
+                font-size: 1.3rem;
+                color: var(--light3);
+                &::first-line{
+                    font-size: 1.4rem;
+                    font-weight: bold;
+                }
+            }
+        }
+        > .right{
+            width: 40%;
+            height: 100%;
+            display: flex;
+            flex-direction: column;
+            > .top{
+                width: 100%;
+                height: 70%;
+                display: flex;
+                justify-content: space-evenly;
+                align-items: center;
+                > .img1{
+                    border: 4px solid var(--dark);
+                    width: 45%;
+                    height: 90%;    
+                    border-top-right-radius: 120px;
+                    border-top-left-radius: 10px;
+                    border-bottom-left-radius: 120px;
+                    border-bottom-right-radius: 10px;
+                    overflow: hidden;
+                    mix-blend-mode: lighten;
+                    > img{
+                        width: 130%;
+                    }
+                }
+                > .img2{
+                    border: 4px solid var(--dark);
+                    width: 45%;
+                    height: 90%;
+                    border-top-left-radius: 120px;
+                    border-top-right-radius: 120px;
+                    border-bottom-right-radius: 120px;
+                    border-bottom-left-radius: 120px;
+                    overflow: hidden;
+                    mix-blend-mode: lighten;
+                    > img{
+                        height: 100%;
+                    }
+                }
+            }
+            > .bottom{
+                width: 100%;
+                height: 30%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                > .img3{
+                    border: 4px solid var(--dark);
+                    width: 90%;
+                    height: 80%;
+                    border-bottom-left-radius: 100px;
+                    border-top-right-radius: 100px;
+                    overflow: hidden;
+                    mix-blend-mode: lighten;
+                    > img{
+                        width: 110%;
+                    }
+                }
+            }
+        }
+    }
+    
+`;
+
 function Home() {
     return ( 
-        <section>
-            
-        </section>
+        <StyledSection>
+            <div className="container">
+                <div className="left">
+                    <p>Welcome to our programming forum, where bytes meet brilliance! Whether you're a seasoned coder or just starting out, join our vibrant community to discuss languages, algorithms, and the latest tech trends. Let's debug together, share insights, and turn lines of code into innovative solutions. Happy coding!</p>
+                </div>
+                <div className="right">
+                    <div className="top">
+                        <div className="img1">
+                            <img src="https://media.istockphoto.com/id/1077503796/photo/young-woman-is-thinking-about-the-statistics.jpg?s=612x612&w=0&k=20&c=yiNNfuNEr8Y-Pb0x-hLKL6W8i3ZwrBXrk2l9Gfsj2ZE=" alt="programing" />
+                        </div>
+                        <div className="img2">
+                            <img src="https://cdn.create.vista.com/api/media/small/6216675/stock-photo-code-of-php-language-programming" alt="programing" />
+                        </div>
+                    </div>
+                    <div className="bottom">
+                        <div className="img3">
+                            <img src="https://www.shutterstock.com/image-photo/millennial-african-employee-sit-office-260nw-1746359810.jpg" alt="programing" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </StyledSection>
      );
 }
 
